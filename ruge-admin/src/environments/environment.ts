9dc396ca3de85c53a2d2d68682dfba3e259485fb
Jorge <sezujo@gmail.com>
2026-05-07 16:33:41 -0600

    env

--- ruge-admin/src/environments/environment.ts
+++ ruge-admin/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7192/api',
-  baseUrl: 'http://localhost:4200',
+  production: true,
+  apiUrl: 'https://rugeapi-production.up.railway.app/api',
+  baseUrl: 'https://ruge-frontend.vercel.app',
   checkinPin: '1234',
   r2PublicUrl: 'https://pub-871cfbe0eb2f4139a30018e262daca76.r2.dev'
 };
+
